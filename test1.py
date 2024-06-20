ed889baaeb683c48c4fb3c73dce9f7581aa3f7d1
ouladsaidkhadidja <155170235+ouladsaidkhadidja@users.noreply.github.com>
2024-06-20 13:11:11 +0100

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+
+print("programing in pythooon...!")
+name="khadidja"
+print(name)
+social = [ "facebook", "instagram", "snapchat"]
+social.append("whatsup")
+social.remove("instagram")
+social.sort()
+print(social)
+len(social)
+print(len(social))
+
+
+
